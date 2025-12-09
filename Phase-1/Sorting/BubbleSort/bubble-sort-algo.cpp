c6ecc5b82e3e814068b4d2ab3620ca9c418d04ad
mh-shihan <mahmodul.no.1@gmail.com>
2025-12-09 08:27:18 +0600

    Write bubble sort

---
+++ Phase-1/Sorting/BubbleSort/bubble-sort-algo.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 9, 1, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubble_sort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
