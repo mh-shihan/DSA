e65dabd4ec724ddcf4d2c3d1d6cd0c6c4d966a04
mh-shihan <mahmodul.no.1@gmail.com>
2026-01-01 12:48:05 +0600

    Implement Heap data structure with insertion, deletion, and printing functionalities

--- Phase-1/Sorting/HeapSort/heap.cpp
+++ Phase-1/Sorting/HeapSort/heap.cpp
@@ -69,6 +69,24 @@ class Heap {
     }
 };
 
+
+void heapify(int arr[], int n, int i) {
+    int largest = i; 
+    int left = 2 * i; 
+    int right = 2 * i + 1; 
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
 int main() {
     Heap h;
 
@@ -82,6 +100,19 @@ int main() {
 
     h.deleteFromHeap();
     h.print();
+
+    int arr[] = {-1, 54, 53, 55, 52, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = n/2; i >= 1; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    cout << "Array representation of Heap after heapify: ";
+    for (int i = 1; i < n; i++) {
+        cout << arr[i] << " ";
+    }cout << endl;
+    
 
     return 0;
 }
