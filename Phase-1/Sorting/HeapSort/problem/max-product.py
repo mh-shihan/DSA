b4b5f21cebb635a4867772ec75a9d22beab5c1a0
mh-shihan <mahmodul.no.1@gmail.com>
2026-02-04 13:33:33 +0600

    Fix import statement for List in maxProduct function

--- Phase-1/Sorting/HeapSort/problem/max-product.py
+++ Phase-1/Sorting/HeapSort/problem/max-product.py
@@ -1,4 +1,7 @@
-  def maxProduct(self, nums: List[int]) -> int:
+from ast import List
+
+
+def maxProduct(self, nums: List[int]) -> int:
         ans = 0
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
