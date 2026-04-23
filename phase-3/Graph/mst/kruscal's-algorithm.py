82ee33b8e0f5421273c0291e84ca6eb319b808c2
mh-shihan <mahmodul.no.1@gmail.com>
2026-04-23 08:03:44 +0600

    Implement Kruskal's algorithm for minimum spanning tree; enhance Disjoint Set methods to return success status

--- phase-3/Graph/mst/kruscal's-algorithm.py
+++ phase-3/Graph/mst/kruscal's-algorithm.py
@@ -1,9 +1,9 @@
-# self.parent = list(range(n + 1))
+from typing import List
 
-class DisjointSet: 
+class DisjointSet:
     def __init__(self, n):
-        self.rank = [0]*(n +1)
-        self.size = [1]*(n + 1)
+        self.rank   = [0] * (n + 1)
+        self.size   = [1] * (n + 1)
         self.parent = [i for i in range(n + 1)]
 
     # PATH COMPRESSION
@@ -14,13 +14,11 @@ class DisjointSet:
         return self.parent[node]
 
     # UNION BY RANK
-    def union_by_rank(self, u, v): 
+    def union_by_rank(self, u, v):
         ulp_u = self.find_ult_parent(u)
         ulp_v = self.find_ult_parent(v)
-
-        if(ulp_u == ulp_v):
+        if ulp_u == ulp_v:
             return False
-        
         if self.rank[ulp_u] < self.rank[ulp_v]:
             self.parent[ulp_u] = ulp_v
         elif self.rank[ulp_v] < self.rank[ulp_u]:
@@ -31,38 +29,33 @@ class DisjointSet:
         return True
 
     # UNION BY SIZE
-    def union_by_size(self, u, v): 
+    def union_by_size(self, u, v):
         ulp_u = self.find_ult_parent(u)
         ulp_v = self.find_ult_parent(v)
-
-        if(ulp_u == ulp_v):
+        if ulp_u == ulp_v:
             return False
-        
-        if(self.size[ulp_u] < self.size[ulp_v]):
+        if self.size[ulp_u] < self.size[ulp_v]:
             self.parent[ulp_u] = ulp_v
-            self.size[ulp_v] += self.size[ulp_u]
+            self.size[ulp_v]  += self.size[ulp_u]
         else:
             self.parent[ulp_v] = ulp_u
-            self.size[ulp_u] += self.size[ulp_v]
+            self.size[ulp_u]  += self.size[ulp_v]
         return True
 
 
-# Example usage
-ds = DisjointSet(7)
-ds.union_by_rank(1, 2)
-ds.union_by_rank(2, 3)
-ds.union_by_rank(4, 5)
-ds.union_by_rank(6, 7)
-ds.union_by_rank(5, 6)
+class Solution:
+    def kruskalsMST(self, V: int, edges: List[List[int]]) -> int:
+        edges.sort(key=lambda x: x[2])   # sort by weight  O(E log E)
 
-if ds.find_ult_parent(3) == ds.find_ult_parent(7):
-    print("Same")
-else:
-    print("Not same")
+        ds         = DisjointSet(V)
+        mst_weight = 0
+        mst_edges  = []
 
-ds.union_by_rank(3, 7)
+        for u, v, w in edges:           
+            if ds.union_by_rank(u, v):
+                mst_weight += w
+                mst_edges.append((u, v, w))
+                if len(mst_edges) == V - 1:
+                    break
 
-if ds.find_ult_parent(3) == ds.find_ult_parent(7):
-    print("Same")
-else:
-    print("Not same")
+        return mst_weight
