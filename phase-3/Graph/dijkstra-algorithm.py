0c6628a03a14ef01fc582d3f15134acc122eb3a5
mh-shihan <mahmodul.no.1@gmail.com>
2026-05-01 18:00:53 +0600

    Implement Dijkstra's algorithm for shortest path; add adjacency list construction and priority queue handling; include visual aids

---
+++ phase-3/Graph/dijkstra-algorithm.py
@@ -0,0 +1,23 @@
+import heapq
+
+class Solution:
+    def dijkstra(self, V, edges, src):
+        # Build adjacency dictionary
+        adj = {i: [] for i in range(V)}
+        for u, v, w in edges:
+            adj[u].append((v, w))
+            adj[v].append((u, w))  # undirected graph
+        
+        dist = [1e9] * V
+        dist[src] = 0
+        pq = [(0, src)]
+        
+        while pq:
+            dis, node = heapq.heappop(pq)
+            
+            for adj_node, edge_weight in adj[node]:
+                if dis + edge_weight < dist[adj_node]:
+                    dist[adj_node] = dis + edge_weight
+                    heapq.heappush(pq, (dist[adj_node], adj_node))
+        
+        return dist
